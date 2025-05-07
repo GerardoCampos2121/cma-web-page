7207377e1e3a5fa415b204b408d048d09ca76d71
desarrolloCanal12 <desarrollo@canal12.com.sv>
2025-05-07 11:04:18 -0600

    cambio a php

--- cmasolutions/contact.php
+++ cmasolutions/contact.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="irstheme">
 
-    <title> Nictorys - Factory & Industrial HTML Template </title>
+    <title> CMA Soluciones S.A. de C.V. </title>
     
     <link href="assets/css/themify-icons.css" rel="stylesheet">
     <link href="assets/css/flaticon.css" rel="stylesheet">
@@ -35,7 +35,7 @@
     <!-- start page-wrapper -->
     <div class="page-wrapper">
 
-        <!-- start preloader -->
+        <!-- start preloader >
         <div class="preloader">
             <div class="lds-ellipsis">
                 <div></div>
@@ -44,136 +44,10 @@
                 <div></div>
             </div>
         </div>
-        <!-- end preloader -->
+        < end preloader -->
 
         <!-- Start header -->
-        <header id="header" class="site-header header-style-1">
-            <div class="topbar">
-                <div class="container">
-                    <div class="row">
-                        <div class="col col-md-9">
-                            <div class="contact-info">
-                                <ul>
-                                    <li><i class="ti-mobile"></i> +884574156414</li>
-                                    <li><i class="ti-email"></i> example@demo.com</li>
-                                    <li><i class="ti-time"></i> Sun - Fri 10AM - PM</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col col-md-3">
-                            <div class="social">
-                                <ul>
-                                    <li><a href="#"><i class="ti-facebook"></i></a></li>
-                                    <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
-                                    <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                                    <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- end topbar -->
-            <nav class="navigation navbar navbar-default">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="open-btn">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt></a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse navbar-right navigation-holder">
-                        <button class="close-navbar"><i class="ti-close"></i></button>
-                        <ul class="nav navbar-nav">
-                            <li class="menu-item-has-children">
-                                <a href="#">Home</a>
-                                <ul class="sub-menu">
-                                    <li><a href="index.html">Home default</a></li>
-                                    <li><a href="index-2.html">Home style 2</a></li>
-                                    <li><a href="index-3.html">Home style 3</a></li>
-                                    <li class="menu-item-has-children">
-                                        <a href="#Level3">Third level</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="#">Level3</a></li>
-                                            <li><a href="#">Level3</a></li>
-                                            <li><a href="#">Level3</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">Pages</a>
-                                <ul class="sub-menu">
-                                    <li><a href="about.html">About</a></li>
-                                    <li><a href="testimonials.html">Testimonials</a></li>
-                                    <li><a href="team.html">Team</a></li>
-                                    <li><a href="faq.html">FAQ</a></li>
-                                    <li><a href="404.html">404</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">Services</a>
-                                <ul class="sub-menu">
-                                    <li><a href="services.html">Services</a></li>
-                                    <li><a href="services-s2.html">Services style 2</a></li>
-                                    <li><a href="service-single.html">Power & energy</a></li>
-                                    <li><a href="mechanical-works.html">Mechanical Works</a></li>
-                                    <li><a href="petroleum-refinery.html">Petroleum Refinery</a></li>
-                                    <li><a href="oil-gas-industry.html">Oil & Gas Industry</a></li>
-                                    <li><a href="logistics-services.html">Logistics Services</a></li>
-                                    <li><a href="general-insustry.html">General Insustry</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">Projects</a>
-                                <ul class="sub-menu">
-                                    <li><a href="projects.html">Projects</a></li>
-                                    <li><a href="projects-s2.html">Projects style 2</a></li>
-                                    <li><a href="portfolio-single.html">Project single</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">Blog</a>
-                                <ul class="sub-menu">
-                                    <li><a href="blog.html">Blog default</a></li>
-                                    <li><a href="blog-left-sidebar.html">Blog left sidebar</a></li>
-                                    <li><a href="blog-full-width.html">Blog full width</a></li>
-                                    <li class="menu-item-has-children">
-                                        <a href="#Level3">Blog single</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="blog-single.html">Blog single</a></li>
-                                            <li><a href="blog-single-left-sidebar.html">Blog single left sidebar</a></li>
-                                            <li><a href="blog-single-full-width.html">Blog single full width</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div><!-- end of nav-collapse -->
-
-                    <div class="search-contact">
-                        <div class="header-search-form-wrapper">
-                            <button class="search-toggle-btn"><i class="ti-search"></i></button>
-                            <div class="header-search-form">
-                                <form>
-                                    <div>
-                                        <input type="text" class="form-control" placeholder="Search here...">
-                                        <button type="submit"><i class="ti-search"></i></button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="contact-btn">
-                            <a href="#" class="theme-btn-s2">Get a quote</a>
-                        </div>
-                    </div>
-                </div><!-- end of container -->
-            </nav>
-        </header>
+        <?php include_once("cabecera.php"); ?>
         <!-- end of header -->
 
 
